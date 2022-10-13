6e1c8cc338e3b0776fcba0099de22f662e4eb3e1
Matthew DuPree <mjdupree.jd@gmail.com>
2022-10-13 16:03:22 -0400

    Game On!
    Game on code with comments

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+//uses input output library
+#include <iostream>
+
+//makes it so that std is implied
+using namespace std;
+
+//the program to run
+int main()
+{
+	//cout == console out == print command
+	//"Game On!" is the string i want to print onto the screen
+	//endl == end line == stops the program from printing that line
+	cout << "Game On!" << endl;
+
+	//stops the program
+	return 0;
+}
